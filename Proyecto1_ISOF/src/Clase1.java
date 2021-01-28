e356c888911f662eda40a61edb0a3389712e0022
docencia <ronnycaiza2@gmail.com>
2021-01-28 13:57:56 +0100

    Nueva funcionalidad en master

--- Proyecto1_ISOF/src/Clase1.java
+++ Proyecto1_ISOF/src/Clase1.java
@@ -1,9 +1,13 @@
 
-public class Clase1 {
-
-	public static void main(String[] args) {
-		System.out.println("Hello, this is a NEW message");
-
+public class Clase1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hola, Vamos a liarla parda");
 	}
-
+	
+	public static void otroMetodo()
+	{
+		System.out.print("Otro metodo");
+	}
 }
